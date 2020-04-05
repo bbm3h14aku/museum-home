d3449005b075c61ffe2e79533873a79165e098b4
Felix <Felix_Jung@web.de>
2020-04-05 19:48:49 +0200

    changing baseelement

---
+++ Assets/BundledAssets/baseelements/Script/Exponat.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exponat : MonoBehaviour
+{
+    public GameObject hiddenChild;
+    public GameObject trigger;
+    public string label;
+    public int id;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
